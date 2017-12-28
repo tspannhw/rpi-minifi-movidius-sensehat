7616ea1aac4b4cae284a639f794d3e945f4f6951
Timothy Spann <tspann@hortonworks.com>
2017-12-28 14:56:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
- #!/bin/bash
+#!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam -r 1280x720 --no-banner /opt/demo/images/$DATE.jpg
+fswebcam -q -r 1280x720 --no-banner /opt/demo/images/$DATE.jpg
 
-python3 -W ignore image-classifier2.py /opt/demo/images/$DATE.jpg
+python3 -W ignore ~/workspace/ncappzoo/apps/image-classifier/image-classifier2.py /opt/demo/images/$DATE.jpg
